dce1f9ecfc445eb335cebc71fb2956143d897899
Justin Waterman <jw2528783@users.noreply.github.com>
2017-10-02 21:12:14 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,88 +1,99 @@
-//Justin Waterman
-//006009948
-//CSE 202
-//IntArray Project
-
 #include <iostream>
 #include <cassert>
 #include <algorithm>
 
 using namespace std;
 
-class IntArray {
-    public: 
-            IntArray(int size);
-            ~IntArray();
-            int & operator[](int i) const;
-            int size() const;
-            void fill(int k);
-            int * begin();
-            int * end();
-
-    private:
-            int len;
-            int * data;
+class Animal {
+	public:
+	    virtual string speak() const = 0;
 };
 
-IntArray::IntArray(int size){
-    len=size;
-    data=new int[len];
+class Cat : public Animal {
+	public: 
+		virtual string speak() const;
+};
+
+class Dog : public Animal {
+	public: 
+		virtual string speak() const;
+};
+
+string Cat::speak() const {
+	return "meow";
 }
 
-IntArray::~IntArray(){
-    delete [] data;
+string Dog::speak() const {
+	return "bark";
 }
 
-int & IntArray::operator [](int i) const{
-    return data[i];
-}
 
-int IntArray::size() const{
-    return len;
-}
+template <class T>
+class Array {
+	public: 
+		Array(int size);
+		~Array();
+		T & operator[](int i) const;
+		int size() const;
+		void fill(int k);
+		T * begin();
+		T * end();
 
-void IntArray::fill(int k){
-    for(int i=0;i<len;i++){
-        data[i]=k;
-    }
-}
+	private:
+		int len;
+		T * data;
+};
 
-int * IntArray::begin(){
+
+template <class T>
+Array<T>::Array(int size){
+	len=size;
+	data=new T[len];
+};
+
+template <class T>
+Array<T>::~Array(){
+	delete [] data;
+};
+
+template <class T>
+T & Array<T>::operator[](int i) const{
+	return data[i];
+};
+
+template <class T>
+int Array<T>::size() const{
+	return len;
+};
+
+template <class T>
+void Array<T>::fill(int k){
+	for(int i=0;i<len;i++){
+		data[i]=k;
+	}
+};
+
+
+template <class T>
+T * Array<T>::begin(){
     return & data[0];
 }
 
-int * IntArray::end(){
+template <class T>
+T * Array<T>::end(){
     return & data[len];
 }
 
-int main(int argc, char** argv) {
-    IntArray a(3);
-    a[0] = 10;
-    a[1] = 11;
-    a[2] = 12;
-    assert(a[0] == 10);
-    assert(a[1] == 11);
-    assert(a[2] == 12);
-    assert(a.size() == 3);
-    a.fill(5);
-    assert(a[0] == 5);
-    assert(a[1] == 5);
-    assert(a[2] == 5);
-    a[0] = 3;
-    a[1] = 1;
-    a[2] = 2;
-    sort(a.begin(), a.end());
-    assert(a[0] == 1);
-    assert(a[1] == 2);
-    assert(a[2] == 3);
-    int * it = a.begin();
-    assert(*it == 1);
-    ++it;
-    assert(*it == 2);
-    ++it;
-    assert(*it == 3);
-    ++it;
-    assert(it == a.end());
 
-    cout << "All tests passed." << endl;
+int main(int argc, char * args[]) {
+
+	Array<Animal *> a(2);
+	a[0] = new Cat();
+	a[1] = new Dog();
+	a[2] = new Cat();
+
+	cout<<a[1]->speak()<<endl;
+	cout<<a[2]->speak()<<endl;
+
 }
+
